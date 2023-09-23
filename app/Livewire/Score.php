20efeb45883ba9fb4cbc8507ec9e760911e6ca5f
Marcos Coelho <mviniciusca@gmail.com>
2023-09-23 20:29:53 -0300

    wip

--- app/Livewire/Score.php
+++ app/Livewire/Score.php
@@ -6,26 +6,54 @@ use Livewire\Component;
 
 class Score extends Component
 {
+    public string $name = '';
     public int $homeScore = 0;
-    public int $visitScore = 0;
+    public int $guestScore = 0;
+    public string $result = '';
+
     public function render()
     {
-        return view('livewire.score');
+        return view('livewire.score', [
+            'team_name'   => $this->name,
+            'home_score'  => $this->homeScore,
+            'guest_score' => $this->guestScore,
+            'result'      => $this->checkScore(),
+        ]);
     }
 
-    public function increment()
+    public function increment(string $vote)
     {
-        //
+        if ($vote === 'guest' && $this->guestScore < 3 && $this->homeScore < 3) {
+            $this->guestScore++;
+        }
+        if ($vote === 'home' && $this->homeScore < 3 && $this->guestScore < 3) {
+            $this->homeScore++;
+        }
     }
 
-    public function decrement()
+    public function decrement(string $vote)
     {
-        //
+        if ($vote === 'guest' && $this->guestScore > 0 && $this->homeScore < 3) {
+            $this->guestScore--;
+        }
+        if ($vote === 'home' && $this->homeScore > 0 && $this->guestScore < 3) {
+            $this->homeScore--;
+        }
     }
 
-    public function scoreCalculator()
+    public function checkScore()
     {
-        //
+        if ($this->guestScore === 3) {
+            $this->result = 'Guest Winner';
+        } elseif ($this->guestScore === $this->homeScore) {
+            $this->result = 'Draw';
+        } elseif ($this->homeScore === 3) {
+            $this->result = 'Home Winner';
+        } elseif ($this->homeScore > $this->guestScore) {
+            $this->result = 'Home Team Average';
+        } else {
+            $this->result = 'Guest Team Average';
+        }
     }
 
 }
--- resources/views/livewire/score.blade.php
+++ resources/views/livewire/score.blade.php
@@ -1,23 +1,54 @@
 <div>
+    <div class="text-3xl uppercase font-bold mb-8">Overwatch League 2023</div>
     <div class="max-w-xs uppercase">
         <div>
             <div class="bg-gray-50 text-slate-500 p-2 text-sm flex justify-between items-center">
-                <div class="p-4 bg-slate-200 rounded "></div>
-                <div></div>
-                <div class="border-slate-200 border py-2 px-3 bg-slate-50 text-center text-slate-500 text-xl">
-
+                <div class="p-1 bg-slate-50 border-slate-300 border rounded ">
+                    <img class="w-10 h-10" src="https://pickem-cdn.overwatchleague.com/teams/hangzhou-spark.svg" />
+                </div>
+                <div id="teamName">Guest Team</div>
+                <div id="teamScore"
+                    class="border-slate-200 border w-10 p-2 bg-slate-50 text-center text-slate-500 text-xl">
+                    {{ $guest_score }}
                 </div>
                 <div class="flex gap-1">
-                    <div wire:click='decrement'
+                    <div wire:click="decrement('guest')"
                         class="bg-slate-300 px-2 rounded hover:opacity-80 cursor-pointer items-center text-slate-500 text-xl">
                         -
                     </div>
-                    <div wire:click='increment'
+                    <div wire:click="increment('guest')"
                         class="bg-slate-300 px-2 rounded hover:opacity-80 cursor-pointer items-center text-slate-500 text-xl">
                         +
                     </div>
                 </div>
             </div>
+            <div class="bg-gray-50 text-slate-500 p-2 text-sm flex justify-between items-center">
+                <div class="p-1 bg-slate-50 border-slate-300 border rounded ">
+                    <img class="w-10 h-10" src="https://pickem-cdn.overwatchleague.com/teams/houston-outlaws.svg" />
+                </div>
+                <div id="teamName">Home Team</div>
+                <div id="teamScore"
+                    class="border-slate-200 border w-10 p-2 bg-slate-50 text-center text-slate-500 text-xl">
+                    {{ $home_score }}
+                </div>
+                <div class="flex gap-1">
+                    <div wire:click="decrement('home')"
+                        class="bg-slate-300 px-2 rounded hover:opacity-80 cursor-pointer items-center text-slate-500 text-xl">
+                        -
+                    </div>
+                    <div wire:click="increment('home')"
+                        class="bg-slate-300 px-2 rounded hover:opacity-80 cursor-pointer items-center text-slate-500 text-xl">
+                        +
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="bg-green-400 inline-block text-center rounded text-xs p-1 text-green-50">
+            {{ $result }}
         </div>
     </div>
+
+
 </div>
