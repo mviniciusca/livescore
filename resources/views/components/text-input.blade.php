52acbdefed9b05bddc633edd118c01667e975c26
Marcos Coelho <mviniciusca@gmail.com>
2023-10-12 17:56:28 -0300

    dark mode updated

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,6 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' =>
+'border-slate-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md
+shadow-sm dark:bg-zinc-950 dark:border-black dark:focus:ring-zinc-800
+dark:text-zinc-500 dark:font-semibold']) !!}>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,22 +5,19 @@
 
     <div class="">
         <div class="mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div
-                class="p-4 sm:p-8 bg-white dark:bg-zinc-800 dar shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white dark:bg-zinc-900">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div
-                class="p-4 sm:p-8 bg-white dark:bg-zinc-800 dar shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white dark:bg-zinc-900">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div
-                class="p-4 sm:p-8 bg-white dark:bg-zinc-800 dar shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white dark:bg-zinc-900">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
